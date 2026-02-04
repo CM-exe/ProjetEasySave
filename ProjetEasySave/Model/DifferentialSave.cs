b4b7844a8e1b88a2f61d1540646dc3a47dd6e195
cmourgue <mourgue.clement+github@gmail.com>
2026-02-04 17:24:27 +0100

    feat(Save): Ajout de l'interface ISaveStrategy et du squelette de la classe DifferentialSave

---
+++ ProjetEasySave/Model/DifferentialSave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetEasySave.Model
+{
+    internal class DifferentialSave : ISaveStrategy
+    {
+        /*
+         * Differential Save implementation of the ISaveStrategy interface.
+         */
+
+        // doSave method implementation for Differential Save
+        public bool doSave(string sourcePath, string destinationPath)
+        {
+            return true;
+        }
+    }
+}
---
+++ ProjetEasySave/Model/ISaveStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetEasySave.Model
+{
+    interface ISaveStrategy
+    {
+        /*
+         * Interface for save strategies
+         */
+
+        // Interface methods
+        bool doSave(string sourcePath, string destinationPath);
+    }
+}
