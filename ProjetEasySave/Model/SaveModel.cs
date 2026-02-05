6930ace70c372a974ef238a6cd9ec18e7bb709af
iamnoariel <noa.pierre203@gmail.com>
2026-02-05 09:40:40 +0100

    feat: ajouter SaveModel

---
+++ ProjetEasySave/Model/SaveModel.cs
@@ -0,0 +1,11 @@
+namespace ProjetEasySave;
+
+public class SaveModel
+{
+    private SaveSpace[] _saveJobs;
+
+    public void importSaveJobs()
+    {
+        throw new NotImplementedException();
+    }
+}
