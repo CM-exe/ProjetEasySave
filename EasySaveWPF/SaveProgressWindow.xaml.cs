7eea852b494e55184020ce21ec00cdac85d05d4f
iamnoariel <noa.pierre203@gmail.com>
2026-02-23 11:03:56 +0100

    fix: Fenêtre SaveProgress affiche le fichier et un message de conclusion. TODO: fix le mécanisme des boutons

---
+++ EasySaveWPF/SaveProgressWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ProjetEasySave.ViewModel;
+using System.Windows;
+
+namespace EasySaveWPF
+{
+    public partial class SaveProgressWindow : Window
+    {
+        private readonly ViewModel _viewModel;
+        private readonly string _saveSpaceName;
+
+        public SaveProgressWindow(ViewModel viewModel, string saveSpaceName)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+            _saveSpaceName = saveSpaceName;
+            DataContext = _viewModel;
+
+            Loaded += SaveProgressWindow_Loaded;
+
+            _ = _viewModel.StartSaveAsync(_saveSpaceName);
+        }
+
+        private async void SaveProgressWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await _viewModel.StartSaveAsync(_saveSpaceName);
+
+                MessageBox.Show(
+                    "Sauvegarde terminée avec succès",
+                    "EasySave",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                );
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show(
+                    "Sauvegarde interrompue",
+                    "EasySave",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.ResumeSave(_saveSpaceName);
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.PauseSave(_saveSpaceName);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.StopSave(_saveSpaceName);
+        }
+    }
+}
